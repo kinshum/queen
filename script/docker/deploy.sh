b84cb11ae3599646cb04899397fc665826800be9
shenjian <shenjian26@hotmail.com>
2020-04-14 18:11:39 +0800

    test

--- script/docker/deploy.sh
+++ script/docker/deploy.sh
@@ -1,7 +1,7 @@
-
+#!/usr/bin/env bash
 #使用说明，用来提示输入参数
 usage() {
-	echo "Usage: sh 执行脚本.sh [port|mount|base|modules|stop|rm|rmiNoneTag]"
+	echo "Usage: sh 执行脚本.sh [port|mount|base|modules|rm|rmiNoneTag|stop_base|stop_modules]"
 	exit 1
 }
 
@@ -45,10 +45,17 @@ modules(){
 	docker-compose up -d queen-gateway1 queen-gateway2 queen-auth1 queen-auth2 queen-user queen-desk queen-system1 queen-system2 queen-log
 }
 
-#关闭所有模块
-stop(){
-	docker-compose stop
+#关闭base模块
+stop_base(){
+	docker-compose stop nacos sentinel web-nginx queen-nginx queen-redis
 }
+
+
+#关闭modules模块
+stop_modules(){
+	docker-compose stop queen-gateway1 queen-gateway2 queen-auth1 queen-auth2 queen-user queen-desk queen-system1 queen-system2 queen-log
+}
+
 
 #删除所有模块
 rm(){
@@ -74,14 +81,17 @@ case "$1" in
 "modules")
 	modules
 ;;
-"stop")
-	stop
-;;
 "rm")
 	rm
 ;;
 "rmiNoneTag")
 	rmiNoneTag
+;;
+"stop_base")
+	stop_base
+;;
+"stop_modules")
+	stop_modules
 ;;
 *)
 	usage
